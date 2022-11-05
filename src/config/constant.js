adb9bf221f6b44c954c87fdf848d99951e089f9d
App Generator <51070104+app-generator@users.noreply.github.com>
2022-11-05 13:04:27 +0200

    Update API_URL management (loaded from ENV)

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,1 +1,8 @@
-export const API_SERVER = "http://localhost:5000/api/";
+let BACKEND_SERVER = null;
+if (process.env.REACT_APP_BACKEND_SERVER) {
+  BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
+} else {
+  BACKEND_SERVER = "http://localhost:5000/api/";
+}
+
+export const API_SERVER = BACKEND_SERVER;
